0fc219b43ff6f67d0c04bfebd5fa78e31f318217
Vincent <vincent@keytelematics.com>
2022-02-02 21:07:33 +0200

    more changes to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,17 +16,17 @@ const Header = () => {
 
     return <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#"><img src={logo} className="App-logo" alt="logo" />Panic System</a>
+            <a className="navbar-brand" href="/"><img src={logo} className="App-logo" alt="logo" />Panic System</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Dashboard</a>
+                        <a className="nav-link" href="/">Dashboard</a>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#" onClick={handleLogout}>Logout</a>
+                        <a className="nav-link" href="/" onClick={handleLogout}>Logout</a>
                     </li>
                 </ul>
             </div>
