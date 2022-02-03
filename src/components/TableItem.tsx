0a4a008f4eaf4d49b47a6a62548b73610848383c
Vincent <vincent@keytelematics.com>
2022-02-03 09:07:45 +0200

    UI changes

--- src/components/TableItem.tsx
+++ src/components/TableItem.tsx
@@ -1,4 +1,6 @@
+import moment from 'moment';
 import React from 'react';
+import { Check } from '@material-ui/icons';
 
 type Props = {
     item: any;
@@ -6,14 +8,17 @@ type Props = {
 };
 
 const TableItem = (props: Props) => {
+    let date = moment(props.item.dateCreated).format('D MMM, YYYY HH:mm A');
+   
+
     return <tr>
-        <th className="align-middle">{props.item.dateCreated}</th>
+        <th className="align-middle">{date}</th>
         <td className="align-middle">{`${props.item.user.firstName} ${props.item.user.lastName}`}</td>
         <td className="align-middle">{props.item.address.country}</td>
         <td className="align-middle">{props.item.address.city}</td>
         <td className="align-middle">{props.item.address.address}</td> 
         <td className="align-middle">{props.item.requestType}</td>
-        <td className="align-middle"><button type="button" className="btn btn-outline-primary" onClick={props.onClick}>Complete</button></td>
+        <td className="align-middle"><button type="button" className="btn btn-outline-success" onClick={props.onClick}><Check /> complete</button></td>
     </tr>
 };
 
