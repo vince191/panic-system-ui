6a12db21ac7bed6c3f7810e03481b771ffbca32a
Vincent <vincent@keytelematics.com>
2022-02-03 07:49:35 +0200

    change

--- src/components/TableItem.tsx
+++ src/components/TableItem.tsx
@@ -9,7 +9,7 @@ const TableItem = (props: Props) => {
     return <tr>
         <th className="align-middle">{props.item.dateCreated}</th>
         <td className="align-middle">{`${props.item.user.firstName} ${props.item.user.lastName}`}</td>
-        <td className="align-middle">{props.item.address}</td>
+        <td className="align-middle">{props.item.address.address}</td>
         <td className="align-middle">{props.item.requestType}</td>
         <td className="align-middle"><button type="button" className="btn btn-outline-primary" onClick={props.onClick}>Complete</button></td>
     </tr>
